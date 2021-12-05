3aca32b798ddafa66b6d076f952f7a328e5c41da
SenX_eb85@itstepru.onmicrosoft.com <SenX_eb85@itstepru.onmicrosoft.com>
2021-12-05 10:58:52 +0500

    Add project files.

---
+++ DinamicMemory/Source.cpp
@@ -0,0 +1,206 @@
+#include<iostream>
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+
+int** allocate(const int rows, const int cols);
+void clear(int** arr, const int rows);
+
+void FillRand(int* arr, const int n, int minRand = 0, int maxRand = 100);
+void FillRand(int** arr, const int rows, const int cols);
+void Print(int* arr, const int n);
+void Print(int** arr, const int rows, const int cols);
+
+int* push_back(int arr[], int& n, int value);
+int* push_front(int arr[], int& n, int value);
+int* pop_back(int arr[], int& n);
+
+int** push_row_back(int** arr, int& rows, const int cols);
+void push_col_back(int** arr, const int rows, int& cols);
+
+//#define DYNAMIC_MEMORY_1
+#define DYNAMIC_MEMORY_2
+
+void main()
+{
+	setlocale(LC_ALL, "");
+#ifdef DYNAMIC_MEMORY_1
+	cout << "Hello" << endl;
+	cout << sizeof("+") << endl;
+	cout << sizeof('+') << endl;
+	int n;	//Размер массива
+	cout << "Введите размер массива: "; cin >> n;
+
+	//Объявление динамического массива:
+	int* arr = new int[n] {};
+
+	FillRand(arr, n);
+	Print(arr, n);
+
+	int value;	//Добавляемое значение
+	cout << "Введите добавляемое значение: "; cin >> value;
+
+	arr = push_back(arr, n, value);
+	Print(arr, n);
+
+	cout << "Введите добавляемое значение: "; cin >> value;
+	arr = push_front(arr, n, value);
+	Print(arr, n);
+
+	arr = pop_back(arr, n);
+	Print(arr, n);
+
+	//Удаление динамического массива:
+	delete[] arr;
+	//Print(arr, n);	//Runtime error  
+#endif // DYNAMIC_MEMORY_1
+
+	int rows;	//Количество строк
+	int cols;	//Количество элементов строки (столбцов)
+	std::cout << "Введите количество строк: "; cin >> rows;
+	cout << "Введите количество элементов строки: "; cin >> cols;
+
+	int** arr = allocate(rows, cols);
+
+	FillRand(arr, rows, cols);
+	Print(arr, rows, cols);
+	cout << "\n-----------------------------------------\n";
+	arr = push_row_back(arr, rows, cols);
+	FillRand(arr[rows - 1], cols, 100, 1000);
+	Print(arr, rows, cols);
+	cout << "\n-----------------------------------------\n";
+	push_col_back(arr, rows, cols);
+	for (int i = 0; i < rows; i++)
+	{
+		arr[i][cols - 1] = rand();
+	}
+	Print(arr, rows, cols);
+
+	clear(arr, rows);
+}
+
+int** allocate(const int rows, const int cols)
+{
+	int** arr = new int* [rows] {};
+	for (int i = 0; i < rows; i++)
+	{
+		arr[i] = new int [cols] {};
+	}
+	return arr;
+}
+void clear(int** arr, const int rows)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		delete[] arr[i];
+	}
+	delete[] arr;
+}
+
+void FillRand(int* arr, const int n, int minRand, int maxRand)
+{
+	//Обращение к элементам динамического массива:
+	for (int i = 0; i < n; i++)
+	{
+		//Через арифметику указателей и оператор разыменования:
+		*(arr + i) = rand() % (maxRand - minRand) + minRand;
+	}
+}
+void FillRand(int** arr, const int rows, const int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			arr[i][j] = rand() % 100;
+		}
+	}
+}
+void Print(int* arr, const int n)
+{
+	for (int i = 0; i < n; cout << arr[i++] << "\t");
+	//{
+	//	//Через оператор индексирования:
+	//	//cout << arr[i] << "\t";
+	//	//Оператор индексирования возвращает значение по индексу.
+	//}
+	cout << endl;
+}
+void Print(int** arr, const int rows, const int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			cout << arr[i][j] << "\t";
+		}
+		cout << endl;
+	}
+}
+
+int* push_back(int arr[], int& n, int value)
+{
+	//1) Создаем буферный массив нужного размера
+	int* buffer = new int[n + 1]{};
+	//2) Копируем содержимое исходного массива в новый (буферный) массив:
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	//3) После того как данные скопированы, исходный массив можно удалить
+	delete[] arr;
+	//4) Подменяем адрес старого массива адресом нового массива:
+	arr = buffer;
+	//5) Только после этого в массиве 'arr' появляется еще один элемент, 
+	//	 в который можно созранить добавляемое значение
+	arr[n] = value;
+	//6) После того как в массиве появился еще один элемент, количество его элементов 
+	//	 увеличилось на 1
+	n++;
+	//Print(arr, n);
+	return arr;
+	//						Mission complete.
+}
+
+int* push_front(int arr[], int& n, int value)
+{
+	int* buffer = new int[n + 1]{};
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i + 1] = arr[i];
+	}
+	delete[] arr;
+	buffer[0] = value;
+	n++;
+	return buffer;
+}
+
+int* pop_back(int arr[], int& n)
+{
+	int* buffer = new int[--n]{};
+	for (int i = 0; i < n; i++)buffer[i] = arr[i];
+	delete[] arr;
+	return buffer;
+}
+
+int** push_row_back(int** arr, int& rows, const int cols)
+{
+	int** buffer = new int* [rows + 1]{};
+	for (int i = 0; i < rows; i++)buffer[i] = arr[i];
+	delete[] arr;
+	buffer[rows] = new int[cols] {};
+	rows++;
+	return buffer;
+}
+void push_col_back(int** arr, const int rows, int& cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		int* buffer = new int[cols + 1]{};
+		for (int j = 0; j < cols; j++)buffer[j] = arr[i][j];
+		delete[] arr[i];
+		arr[i] = buffer;
+	}
+	cols++;
+}
